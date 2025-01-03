69d06da4e969f32d9719f1b144a916bd4804df50
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-01-03 00:23:19 -0800

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+				<ul>
+					<li>
+						<p><strong>Darkeyes Publishing</strong> creates eclectic works of fantasy fiction, short stories, poetry, and&nbsp;
+							<a
+								className={styles.inline} 
+								href="https://dreadlore.com"
+							>
+								tabletop role playing games
+							</a>. Many of these stories are set in the sword and sorcery world of Craster, a dying world with denizens who refuse to go quietly into the night.</p>
+
+						<p>Darkeyes also creates system-agnostic adventure modules and locations which promote Player creativity and characterization.</p>
+					</li>
+					<li>
+						<p>Our newest release is 
+							<a
+								className={styles.inline}
+								href="https://www.lulu.com/shop/bill-bunkum-and-bill-bunkum/falling-asleep/paperback/product-7kg44ky.html?q=dreadlore&page=1&pageSize=4"
+							>
+								“Falling Asleep,”
+							</a> an illustrated short story which follows the ruminations of Daria, a denizen of Craster and The Bonestone City, as she goes about her daily grind and must cope with the dregs of a past relationship.</p> 
+					</li>
+					<li>
+						<p>Upcoming releases will include the novella, “Merriam and the Mark,” a tale of adventure that follows the assassin, Merriam Látast, and the squirrelly situation in which she finds herself.</p>	
+					</li>
+				</ul>
+			</main>
+      <footer className={styles.footer}>
+        <a
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+        	Home 
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+        	Merch 
+        </a>
+				<a
+          href="https://dreadlore.substack.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+        	Newsletter 
+        </a>
+        <a
+          href="https://www.lulu.com/spotlight/darkeyespublishing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to Book Store →
+        </a>
+      </footer>
+
+		</div>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,119 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/assets/iiixi-logo-icon-2020-white.png"
+          alt="darkeyes publishing logo"
+          width={150}
+          height={100}
+          priority
+        />
+				<h2>Darkeyes Publishing</h2>
+        <ul>
+          <li>
+						Check out our titles below.
+          </li>
+        </ul>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://www.lulu.com/shop/bill-bunkum-and-bill-bunkum/falling-asleep/paperback/product-7kg44ky.html?q=dreadlore&page=1&pageSize=4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/assets/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+						Falling Asleep
+          </a>
+          <a
+            href="https://www.lulu.com/shop/bill-bunkum-and-cameron-white-and-timothy-roberts-and-sharon-hurst/dreadlore-corebook-deluxe-hardback/hardcover/product-ekmn4v.html?q=dreadlore"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+          	DreadLore Corebook (hardback)
+          </a>
+          <a
+            href="https://www.lulu.com/shop/bill-bunkum-and-cameron-white-and-sharon-hurst-and-dalton-anderson/dreadlore-corebook-deluxe-paperback/paperback/product-v89w77p.html?q=dreadlore&page=1&pageSize=4"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+          	DreadLore Corebook (paperback)
+          </a>
+
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="/about"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+        	About 
+        </a>
+        <a
+          href="https://www.redbubble.com/people/billbunkum/shop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+        	Merch 
+        </a>
+				<a
+          href="https://dreadlore.substack.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+        	Newsletter 
+        </a>
+        <a
+          href="https://www.lulu.com/spotlight/darkeyespublishing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to Book Store →
+        </a>
+      </footer>
+    </div>
+  );
+}
