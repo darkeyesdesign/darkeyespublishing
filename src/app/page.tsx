62b3408437c09a6ba8f0518a8ce1bb50fce088ab
Bill Bunkum <billbunkum@Billy-Boy-Uno.local>
2025-04-05 16:06:59 -0700

    add other products and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-						Falling Asleep
+						Falling Asleep (short story)
           </a>
 				</div>
 				<div className={styles.ctas}>
@@ -70,8 +70,80 @@ export default function Home() {
           >
           	DreadLore Corebook (paperback)
           </a>
-
         </div>
+
+				<div className={styles.ctas}>
+					<img
+						className={styles.thumbnail}
+						src="./cover-character-sheet.png"
+						alt="Character Sheet"
+						height={300}
+						width={225}
+					/>
+          <a
+            href="https://www.drivethrurpg.com/en/product/432975/dreadlore-character-sheet"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+          	DL Character Sheet (pdf)	
+          </a>
+				</div>
+
+				<div className={styles.ctas}>
+					<img
+						className={styles.thumbnail}
+						src="./cover-2-theurgist-atop-seers-hill-adventure-location-vertical-small-fin.png"
+						alt="The Theurgist Atop Seer's Hill"
+						height={300}
+						width={225}
+					/>
+          <a
+            href="https://www.drivethrurpg.com/en/product/503951/the-theurgist-atop-seer-s-hill-adventure-location"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+          	The Theurgist Atop Seer's Hill (module)	
+          </a>
+				</div>
+
+				<div className={styles.ctas}>
+					<img
+						className={styles.thumbnail}
+						src="./the-keeping-cave-cover.png"
+						alt="The Keeping Cave (module)"
+						height={300}
+						width={225}
+					/>
+          <a
+            href="https://www.drivethrurpg.com/en/product/432251/the-keeping-cave-adventure-module"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+          	The Keeping Cave (module)	
+          </a>
+				</div>
+
+				<div className={styles.ctas}>
+					<img
+						className={styles.thumbnail}
+						src="./gsg-v.1.2-cover.jpg"
+						alt="Getting Started Guide"
+						height={300}
+						width={225}
+					/>
+          <a
+            href="https://www.drivethrurpg.com/en/product/432935/dreadlore-getting-started-guide-updated-v-1-2"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+          	DreadLore Getting Started Guide (pdf)	
+          </a>
+				</div>
+
       </main>
 
       <footer className={styles.footer}>
