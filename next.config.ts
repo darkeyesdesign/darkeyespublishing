498ac55e44b0b4a8b023be0832cf840dc9c055fc
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-04-09 16:26:27 -0700

    change config so CSS works

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: 'export',
+	basepath: '/darkeyespublishing/',
+	assetPrefix: '/darkeyespublishing/',
 };
 
 export default nextConfig;
